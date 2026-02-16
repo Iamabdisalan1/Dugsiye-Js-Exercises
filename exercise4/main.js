5a8e8be0700f4298897cac4602dcba19f99b86d3
Iamabdisalan1 <soomane5188@gmail.com>
2026-02-16 12:23:02 +0300

    submited Exercise 4

---
+++ exercise4/main.js
@@ -0,0 +1,20 @@
+
+// Function declaration
+function Add (a,b) {
+ console.log(a + b)
+}
+Add(6,15)
+Add(5,6)
+Add(7,6)
+Add(6,8)
+Add(1,9)
+// Function Expression
+const add = function(a,b) {
+    console.log(a + b)
+}
+Add(6,15)
+Add(5,6)
+Add(7,6)
+Add(6,8)
+Add(1,9)
+
