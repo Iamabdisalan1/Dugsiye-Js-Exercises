c187d0a2e3133d5e5c7380922cee59d7574893d6
Iamabdisalan1 <soomane5188@gmail.com>
2026-02-19 13:00:28 +0300

    submited Exercise8

---
+++ exwecise8/main.js
@@ -0,0 +1,9 @@
+let car = {
+    make: "Toyota",
+    model: "helux",
+    year: 2018
+};
+
+console.log(car.make); // Output: Toyota
+console.log(car.model); // Output: helux
+console.log(car.year); // Output: 2018
