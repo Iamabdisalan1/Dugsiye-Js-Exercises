babd2e9d32db5280e5d9c12dc7816b3ebebc8e74
Iamabdisalan1 <soomane5188@gmail.com>
2026-02-19 15:23:30 +0300

    submited Exercise9

---
+++ exercise9/main.js
@@ -0,0 +1,14 @@
+let car = {
+    make: "Toyota",
+    model: "helux",
+    year: 2018,   
+    start: () => {
+        console.log("The car has started");
+    }
+};
+
+
+console.log(car.make);
+console.log(car.model);
+console.log(car.year);
+car.start(); 
