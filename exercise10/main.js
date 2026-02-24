c65cd038b032d17fc318c68ed93bed5bbe5090b2
Iamabdisalan1 <soomane5188@gmail.com>
2026-02-24 12:39:25 +0300

    submited exercise10

---
+++ exercise10/main.js
@@ -0,0 +1,9 @@
+let library = [
+    { title: "Don Quixote", author: "Miguel de Cervantes",year: 1605},
+     { title: "A Tale of Two Cities", author: "Charles Dickens",year: 1954}
+   
+];
+
+console.log(library[0].title); // Output: title
+console.log(library[1].author); // Output: author
+
